111639f5d0b7547ffce4befa613dbf012ed2dd9d
Huy Nguyen <quanghuy21988@gmail.com>
2018-11-15 11:51:08 +0700

    test

---
+++ CigServerConsole/CigServerConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CigServerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ahihi");
+            Console.Read();
+        }
+    }
+}
